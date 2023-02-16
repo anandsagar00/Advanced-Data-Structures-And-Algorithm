5faec896205b548370dbadd61765182d0075668f
Anand Sagar <anandsagar152@gmail.com>
2023-02-17 00:40:52 +0530

    Added : Nearest Cell having 1,learnt how BFS can be applied in these scenarios

--- 009 Graph/nearest_cell_having_1.cpp
+++ 009 Graph/nearest_cell_having_1.cpp
@@ -15,7 +15,89 @@ and i2, j2 are the row number and column number of the nearest cell having value
 #include<utility>
 using namespace std;
 
-//TODO : Write algorithm
+vector<vector<int> > nearest(vector<vector<int> > &arr)
+{
+    int r=arr.size();
+    int c=arr[0].size();
+
+    //I'll be storing answer in this vector
+    vector<vector<int> > ans(r,vector<int>(c,0));
+
+    vector<vector<int> > visited(r,vector<int>(c,0));
+
+    //first we will take out the details of coordinates of the 1's in the queue and number of steps it has moved in BFS
+    queue<pair<pair<int,int>,int > > q;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            if(arr[i][j]==1)
+            {
+                q.push(make_pair(make_pair(i,j),0));
+            }
+        }
+    }
+
+    while(!q.empty())
+    {
+        int i=q.front().first.first;
+        int j=q.front().first.second;
+
+        int steps=q.front().second;
+
+        q.pop();
+
+        // we will travel in all 4 directions and as soon as we will find 1 we will add the dist to answer vector
+        
+        //left
+        if(j-1>=0 && visited[i][j-1]==0 && arr[i][j-1]==0)
+        {
+            //inserting into answer
+            ans[i][j-1]=steps+1;
+            //inserting into queue
+            q.push(make_pair(make_pair(i,j-1),steps+1));
+            //marking as visited
+            visited[i][j-1]=1;
+            
+        }
+
+        //right
+        if(j+1<c && visited[i][j+1]==0 && arr[i][j+1]==0)
+        {
+            //inserting into answer
+            ans[i][j+1]=steps+1;
+            //inserting into queue
+            q.push(make_pair(make_pair(i,j+1),steps+1));
+            //marking as visited
+            visited[i][j+1]=1;
+            
+        }
+
+        //down
+
+        if(i+1<r && visited[i+1][j]==0 && arr[i+1][j]==0)
+        {
+            //inserting into answer
+            ans[i+1][j]=steps+1;
+            //inserting into queue
+            q.push(make_pair(make_pair(i+1,j),steps+1));
+            //marking as visited
+            visited[i+1][j]=1;
+        }
+
+        //Up
+        if(i-1>=0 && visited[i-1][j]==0 && arr[i-1][j]==0)
+        {
+            //inserting into answer
+            ans[i-1][j]=steps+1;
+            //inserting into queue
+            q.push(make_pair(make_pair(i-1,j),steps+1));
+            //marking as visited
+            visited[i-1][j]=1; 
+        }
+    }
+    return ans;
+}
 
 int main()
 {
