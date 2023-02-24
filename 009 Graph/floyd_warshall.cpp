447fe53dad486a4f3af3d2e5de9fc50ad1b4932e
Anand Sagar <anandsagar152@gmail.com>
2023-02-24 21:15:19 +0530

    Added : Floyd Warshall

--- 009 Graph/floyd_warshall.cpp
+++ 009 Graph/floyd_warshall.cpp
@@ -31,7 +31,19 @@ void floydWarshall(int **arr,int n)
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
-        distance[i][j]=arr[i][j];
+        {
+            if(arr[i][j]==0 && i!=j)
+            {
+                //nodes unreachable
+                distance[i][j]=INFINITY;
+            }
+            else if(arr[i][j]==0 && i==j)
+            {
+                distance[i][j]=0;
+            }
+            else
+            distance[i][j]=arr[i][j];
+        }
     }
 
     for(int i=0;i<n;i++)//this loop marks the source node
