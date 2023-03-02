3aeda0046ea6f2747511ee8dfe3b7b52a239a21d
Anand Sagar <anandsagar152@gmail.com>
2023-03-03 03:10:21 +0530

    TODO :

---
+++ 009 Graph/articulation_point.cpp
@@ -0,0 +1,13 @@
+//Context : https://youtu.be/j1QDfU21iZk?list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn
+
+// Problem Link : https://practice.geeksforgeeks.org/problems/articulation-point-1/1
+
+// Pre-req : Find all the bridges in a graph , TARZAN's alg 
+
+/*
+This has a slight variation from Bridges's in the graph
+
+also 
+
+0 or first node can never be a articulation point as even though we remove 0th node graph will still have 1 component
+*/
