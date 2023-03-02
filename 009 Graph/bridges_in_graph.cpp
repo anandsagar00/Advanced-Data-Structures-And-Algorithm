a727a61086cde0d54a4e5f63082cd2c663865db7
Anand Sagar <anandsagar152@gmail.com>
2023-03-02 20:13:22 +0530

    TODO :

--- 009 Graph/bridges_in_graph.cpp
+++ 009 Graph/bridges_in_graph.cpp
@@ -1,4 +1,12 @@
+/*
+The video below will teach you a lot about the following :
+Discovery Time , Lowest Discovery Node reachable etc.. which would be useful for Striver's video
+*/
+
+// Tarzan's alg : https://youtu.be/ZeDNSeilf-Y (Just see what is discovery time and what is lowest discovery time node available and nothing else)
+
 // Context : https://youtu.be/qrAub5z8FeA?list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn
 
 // Problem Link : https://leetcode.com/problems/critical-connections-in-a-network/
+
 
