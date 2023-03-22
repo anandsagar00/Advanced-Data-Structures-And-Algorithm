87a65f242d7c972d690742b63898326140d21c6e
Anand Sagar <anandsagar152@gmail.com>
2023-03-22 20:56:20 +0530

    Added : Sum of n natural numbers using recursion

--- 007 Recursion/sum.cpp
+++ 007 Recursion/sum.cpp
@@ -1,20 +1,6 @@
 //Sum of n numbers using recursion
 
 #include<iostream>
-#include<fstream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<utility>
-#include<unordered_map>
-#include<set>
-#include<map>
-#include<unordered_set>
-#include<string>
-#include<limits.h>
 using namespace std;
 #define ll long long int
 const int mod=1e9+7;
