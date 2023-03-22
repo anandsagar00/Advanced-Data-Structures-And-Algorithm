f9b435383d44c234530ea745ae59c78064df6106
Anand Sagar <anandsagar152@gmail.com>
2023-03-22 20:49:49 +0530

    Added : basic printing using recursion

--- 007 Recursion/print_recur.cpp
+++ 007 Recursion/print_recur.cpp
@@ -3,20 +3,6 @@
 //Print 1 to n using recursion
 
 #include<iostream>
-#include<fstream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<utility>
-#include<unordered_map>
-#include<set>
-#include<map>
-#include<unordered_set>
-#include<string>
-#include<limits.h>
 using namespace std;
 #define ll long long int
 const int mod=1e9+7;
--- 007 Recursion/print_recur_reverse.cpp
+++ 007 Recursion/print_recur_reverse.cpp
@@ -3,20 +3,6 @@
 // print n to 1 using recursion
 
 #include<iostream>
-#include<fstream>
-#include<vector>
-#include<iterator>
-#include<algorithm>
-#include<stack>
-#include<queue>
-#include<deque>
-#include<utility>
-#include<unordered_map>
-#include<set>
-#include<map>
-#include<unordered_set>
-#include<string>
-#include<limits.h>
 using namespace std;
 #define ll long long int
 const int mod=1e9+7;
