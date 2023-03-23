d3fbb936be586c55612fa6ebec1c9b015b0a9bed
Anand Sagar <anandsagar152@gmail.com>
2023-03-24 01:01:04 +0530

    Added : Combination Sum

---
+++ 007 Recursion/combination_sum.cpp
@@ -0,0 +1,38 @@
+// Context : https://youtu.be/OyZFFqQtu98?list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
+
+// Problem Link : https://leetcode.com/problems/combination-sum/submissions/
+
+
+class Solution {
+public:
+    
+    void combinationSum(vector<int>& candidates, int target,vector<vector<int>> &ans,vector<int> &temp,int i) {
+        if(i>=candidates.size() || target<0)
+            return;
+        else if(target==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else
+        {
+            //Two cases 
+            
+            //Case 1 don't take the element
+            combinationSum(candidates,target,ans,temp,i+1);
+            
+            //Case 2 take element into consideration 
+            temp.push_back(candidates[i]);
+            combinationSum(candidates,target-candidates[i],ans,temp,i);
+            temp.pop_back();
+        }
+        
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        combinationSum(candidates,target,ans,temp,0);
+        return ans;
+    }
+};
