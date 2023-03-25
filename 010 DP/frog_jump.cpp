0cb781be7846935abbf9a46eafa4a536caff728c
Anand Sagar <anandsagar152@gmail.com>
2023-03-26 01:45:33 +0530

    Added : Frog Jump (Memo)

--- 010 DP/frog_jump.cpp
+++ 010 DP/frog_jump.cpp
@@ -45,7 +45,7 @@ ll minCost(int i, vector<ll> &dp, vector<int> &heights)
         /*
         In case you are worried that height[i-2] may acces negative index and may result in Crashing of code
         then let me clarify that...
-        I am using vector so height[-ve index] will give zero and the cost_steps2 will give INT_MAX
+        I am using vector so height[-ve index] will give zero and the  cost_steps2 will give INT_MAX
         
         or 
 
